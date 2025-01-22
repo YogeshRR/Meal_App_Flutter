0d870f72802ca5442e16e0fc22a4c20da2c491be
Yogesh Raut <hiyogesh.raut@gmail.com>
2025-01-22 22:10:23 +0530

    fine tuning  explicit animation, implemented Slide Transition and Curved Animation.

--- lib/Screens/categories_screen.dart
+++ lib/Screens/categories_screen.dart
@@ -74,9 +74,12 @@ class _CategoriesScreenState extends State<CategoriesScreen>
             ),
         ],
       ),
-      builder: (ctx, child) => Padding(
-          padding:
-              EdgeInsets.only(top: 100 - (_animationController.value * 100)),
+      builder: (ctx, child) => SlideTransition(
+          position: Tween<Offset>(
+            begin: const Offset(0, 0.3),
+            end: const Offset(0, 0),
+          ).animate(CurvedAnimation(
+              parent: _animationController, curve: Curves.easeInOut)),
           child: child),
     );
   }
