522fc516ea23cc35b2a54d6f2c972fd3002111a6
Yogesh Raut <hiyogesh.raut@gmail.com>
2025-01-22 21:14:56 +0530

    identified difference between implicit and explicit animation, mixin and understand difference.

--- lib/Screens/categories_screen.dart
+++ lib/Screens/categories_screen.dart
@@ -6,12 +6,37 @@ import 'package:meals_app/Model/category.dart';
 import 'package:meals_app/widget/category_item.dart';
 import 'package:meals_app/model/meal.dart';
 
-class CategoriesScreen extends StatelessWidget {
+class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({super.key, required this.availableMeals});
   final List<Meal> availableMeals;
 
+  @override
+  State<CategoriesScreen> createState() => _CategoriesScreenState();
+}
+
+class _CategoriesScreenState extends State<CategoriesScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  @override
+  void initState() {
+    // TODO: implement initState
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 300),
+      lowerBound: 0,
+      upperBound: 1,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
   void _selectCategory(BuildContext context, Category category) {
-    final filteredMeals = availableMeals
+    final filteredMeals = widget.availableMeals
         .where(
           (meals) => meals.categories.contains(category.id),
         )
